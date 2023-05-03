7de764d4a9ff9cc0acf4ea0681c9d4301ea6b35c
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-03 11:19:15 +0200

    refactoring logica js snack-3

--- js-snack-3/main.js
+++ js-snack-3/main.js
@@ -1,6 +1,5 @@
 // array di numeri interi (vuoto - da riempire)
 const myNumbersArray = [];
-
 // numero random da 1 a 10 per decidere da quanti elementi sarà costituito il mio array
 const randomNumber = Math.floor(Math.random() * 10 + 1);
 // ciclo in base al numero random di prima i numeri che saranno gli elementi dell'array, e li aggiungo
@@ -13,13 +12,14 @@ for (let i = 0; i < randomNumber; i++) {
 
 console.log(myNumbersArray);
 
+
+let somma = 0;
 // se l'indice dell'elemento dell'array è dispari somma quel numero insieme ai numeri di altre posizioni dispari
-
-
 for (let i = 0; i < myNumbersArray.length; i + 2) {
     const currentElement = myNumbersArray[i];
     if (i % 2 !== 0) {
-        console.log("numeri in posizione dispari: " + currentElement)
+        somma += currentElement;
+        console.log("somma numeri in posizione dispari: " + somma)
     }
 }
 
